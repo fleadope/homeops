0f6b5c4375ca2fc8540b6eb30a63c17d700a4492
fleadope <leepope@gmail.com>
2024-06-13 21:20:04 -0700

    initial configuration

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "fleadope"
 
     workspaces {
       name = "homelab-external"
--- platform/gitea/files/config/go.mod
+++ platform/gitea/files/config/go.mod
@@ -1,4 +1,4 @@
-module git.khuedoan.com/khuedoan/homelab/gitea/config
+module git.leepope.cc/khuedoan/homelab/gitea/config
 
 go 1.19
 
